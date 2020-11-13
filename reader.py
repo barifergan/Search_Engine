e496ccf1cd0df456c735a75fe19ea183fdfeb042
barifergan <62896768+barifergan@users.noreply.github.com>
2020-11-13 10:32:25 +0200

    class reader

--- reader.py
+++ reader.py
@@ -16,3 +16,26 @@ class ReadFile:
         full_path = os.path.join(self.corpus_path, file_name)
         df = pd.read_parquet(full_path, engine="pyarrow")
         return df.values.tolist()
+
+    def open_folder(self, corpus_path):
+        root_dir = corpus_path
+        tweets_list = []
+
+        for subdir, dirs, files in os.walk(root_dir): #iterate trough all inner folders and pulls out all parquet files, send to read_file function
+            for file in files:
+                file_type = file[-8:]
+                if file_type == '.parquet':
+                    self.corpus_path = subdir
+                    tweets_list += self.read_file(file)
+        print(tweets_list)
+        return tweets_list
+
+
+corpus_path1 = "C:\\Users\\barif\\PycharmProjects\\Search_Engine\\Data"
+read = ReadFile(corpus_path1)
+path1 = read.open_folder(corpus_path1)
+
+
+
+
+
