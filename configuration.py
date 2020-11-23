83403cef464edc90ff6b8b26fbfd4e09657c5c57
barifergan <62896768+barifergan@users.noreply.github.com>
2020-11-23 23:43:57 +0200

    finish parser

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 class ConfigClass:
     def __init__(self):
-        self.corpusPath = 'C:\\Users\\ronen\\Downloads\\Data'
+        self.corpusPath = 'C:\\Users\\barif\\PycharmProjects\\Search_Engine\\Data\\date=07-09-2020'
         self.savedFileMainFolder = ''
         self.saveFilesWithStem = self.savedFileMainFolder + "/WithStem"
         self.saveFilesWithoutStem = self.savedFileMainFolder + "/WithoutStem"
